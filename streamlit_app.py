465d890de0ef2594c8eb9fb242df3c45bd4387d9
apmoskal <moskalap@bc.edu>
2025-08-06 19:20:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,7 +154,6 @@ elif crop_selection_drop == "Yams":
     st.write(f"You selected: {crop_selection_drop}")
     st.image("Yam.jpg", width=600)
     st.write("Yams are tropical tuber crops primarily cultivated for human consumption. They thrive in warm climates with consistent temperatures around 27.5°C and require high rainfall ranging from 50 to 100 mm per week. Optimal growth occurs with moderate sunlight exposure of about 6 to 8 hours daily. As root crops, yams are a vital food source in many regions, valued for their nutritional content and versatility in cooking.")
-elif crop_selection_drop == "None Selected":
     
 
 # Only scatter will have selection
